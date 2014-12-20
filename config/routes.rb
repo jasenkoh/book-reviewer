ba079194dcc874d6211a031bde67b2298fe58951
jasenkoh <hjasenko@hotmail.com>
2014-12-20 04:09:35 -0500

    Add genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'finished_books/index'
-
     resources :books
+    resources :genres
     resources :finished_books
 end
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
-    Book.create!(title: 'Pragmatic programmer', rating: 5)
-    Book.create!(title: "Ender's Game", rating: 3)
+    @scifi = Genre.create!(name: 'SF')
+
+    @scifi.books.create!(title: 'Star trek', rating: 5)
+    @scifi.books.create!(title: "Ender's Game", rating: 3)
   end
 
   test 'listing books'do
@@ -12,7 +14,11 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
-    assert_equal Book.count, json(response.body)[:books].size
+    books = json(response.body)[:books]
+    assert_equal Book.count, books.size
+
+    book = Book.find(books.first[:id])
+    assert_equal @scifi.id, book.genre.id
   end
 
   test 'lists top rated books' do
