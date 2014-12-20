01fb452776c2f8c532752a3b86fd9c09bbda1ca2
jasenkoh <hjasenko@hotmail.com>
2014-12-20 13:16:35 -0500

    turn off mangling in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'finished_books/index'
 
   namespace :api do
     resources :books
